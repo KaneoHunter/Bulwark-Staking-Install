50abfabb5b7ef8544ed892e7d83987c1b754ebe4
Kaneo <dingdongkane@gmail.com>
2018-06-25 06:34:36 +0100

    Make script compatible with SHN devices (non-root)

--- refresh.sh
+++ refresh.sh
@@ -1,54 +1,49 @@
 #!/bin/bash
 
+BOOTSTRAPURL="https://github.com/bulwark-crypto/Bulwark/releases/download/1.3.0/bootstrap.dat.xz"
+BOOTSTRAPARCHIVE="bootstrap.dat.xz"
+
 clear
 echo "This script will refresh your wallet."
 read -p "Press Ctrl-C to abort or any other key to continue. " -n1 -s
 clear
 
-if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root."
-  exit 1
-fi
+USER=$(whoami)
+USERHOME=home/$USER
 
-USER=`ps u $(pgrep bulwarkd) | grep bulwarkd | cut -d " " -f 1`
-USERHOME=`eval echo "~$USER"`
+sudo systemctl stop bulwarkd
 
-if [ -e /etc/systemd/system/bulwarkd.service ]; then
-  systemctl stop bulwarkd
-else
-  su -c "bulwark-cli stop" $BWKUSER
-fi
-
-echo "Refreshing wallet, please wait."
+echo "Refreshing node, please wait."
 
 sleep 5
 
-rm -rf $USERHOME/.bulwark/blocks
-rm -rf $USERHOME/.bulwark/database
-rm -rf $USERHOME/.bulwark/chainstate
-rm -rf $USERHOME/.bulwark/peers.dat
+sudo rm -Rf $USERHOME/.bulwark/blocks
+sudo rm -Rf $USERHOME/.bulwark/database
+sudo rm -Rf $USERHOME/.bulwark/chainstate
+sudo rm -Rf $USERHOME/.bulwark/peers.dat
 
-cp $USERHOME/.bulwark/bulwark.conf $USERHOME/.bulwark/bulwark.conf.backup
-sed -i '/^addnode/d' $USERHOME/.bulwark/bulwark.conf
+sudo cp $USERHOME/.bulwark/bulwark.conf $USERHOME/.bulwark/bulwark.conf.backup
+sudo sed -i '/^addnode/d' $USERHOME/.bulwark/bulwark.conf
 
-if [ -e /etc/systemd/system/bulwarkd.service ]; then
-  sudo systemctl start bulwarkd
-else
-  su -c "bulwarkd -daemon" $USER
-fi
+echo "Installing bootstrap file..."
+wget $BOOTSTRAPURL && xz -cd $BOOTSTRAPARCHIVE > $USERHOME/.bulwark/bootstrap.dat && rm $BOOTSTRAPARCHIVE
+
+sudo systemctl start bulwarkd
+
+clear
 
 echo "Your wallet is syncing. Please wait for this process to finish."
 echo "This can take up to a few hours. Do not close this window." && echo ""
 
-until [  $(bulwark-cli getconnectioncount) -gt 0  ] 2>/dev/nul; do
+until [ -n "$(bulwark-cli getconnectioncount 2>/dev/null)"  ]; do
   sleep 1
 done
 
-until su -c "bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" $USER; do
-  echo -ne "Current block: "`su -c "bulwark-cli getinfo" $USER | grep blocks | awk '{print $3}' | cut -d ',' -f 1`'\r'
+until su -c "bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" bulwark; do
+  echo -ne "Current block: "`su -c "bulwark-cli getinfo" bulwark | grep blocks | awk '{print $3}' | cut -d ',' -f 1`'\r'
   sleep 1
 done
 
 clear
 
-echo "Your wallet has been refreshed, synced, and restarted!"
+echo "" && echo "Wallet refresh completed." && echo ""
