6ffd922523f4b3ce4566c654126b5bf7d7924699
Kaneo <dingdongkane@gmail.com>
2018-06-25 17:18:49 +0100

    Changed USER and USERHOME variables.

--- setup.sh
+++ setup.sh
@@ -35,8 +35,8 @@ EXTERNALIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 fi
 clear
 
-USER=root
-USERHOME=`eval echo "~$USER"`
+USER=$(whoami)
+USERHOME=/home/$USER
 
 # Generate random passwords
 RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
