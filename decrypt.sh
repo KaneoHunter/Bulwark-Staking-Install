3e5e2f774d678b142d7a7fe05067aee9f2c2a1e2
Kaneo <dingdongkane@gmail.com>
2018-08-16 16:18:38 +0100

    Added a fi, until > if

--- decrypt.sh
+++ decrypt.sh
@@ -6,9 +6,10 @@ set +o history
 #check bulwarkd is active. activate if not.
 if [ -z "$(ps cax | grep bulwarkd)" ]; then
   systemctl start bulwarkd
+fi
 
 #ask for password.
-read -e -s -p "Please enter a password to decrypt your staking wallet : " ENCRYPTIONKEY
+read -e -s -p "Please enter a password to decrypt your staking wallet (Your password will not show as you type, this is normal) : " ENCRYPTIONKEY
 
 #confirm wallet is synced. wait if not.
 until bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null; do
@@ -17,14 +18,18 @@ until bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : tru
 done
 
 #unlock wallet. confirm it's unlocked.
-if [ -n $(bulwark-cli getstakingstatus | grep walletunlocked | grep false) ]; then
+until [ -n $(bulwark-cli getstakingstatus | grep walletunlocked | grep false) ]; do
+
+  #ask for password and attempt it
+  read -e -s -p "Please enter a password to decrypt your staking wallet (Your password will not show as you type, this is normal) : " ENCRYPTIONKEY
   bulwark-cli walletpassphrase $ENCRYPTIONKEY 99999999 true
-else
-  #tell user all was successful.
-  clear
-  echo "Wallet successfully unlocked!"
-  echo " "
-  bulwark-cli getstakingstatus
+done
+
+#tell user all was successful.
+clear
+echo "Wallet successfully unlocked!"
+echo " "
+bulwark-cli getstakingstatus
 
 #restart history.
 set -o history
